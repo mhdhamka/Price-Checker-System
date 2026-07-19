7f628e244df9897a1c1ebbf94c68c706803202a4
Mohd Hamka <122353752+mhdhamka@users.noreply.github.com>
2026-07-19 19:22:12 +0800

    Update icon link path in loginSelection.php

--- src/public/loginSelection.php
+++ src/public/loginSelection.php
@@ -6,7 +6,7 @@
     <title>Login Selection</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     
-    <link rel="icon" href="assets/images/logo.png" type="image/x-icon">
+    <link rel="icon" href="../../assets/images/logo.png" type="image/x-icon">
     
     <style>
         body {
